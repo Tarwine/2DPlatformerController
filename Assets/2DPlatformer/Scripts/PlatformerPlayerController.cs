1c790330c655fabb44ab2ecae1f9f7008efc5d8c
braustin <brandonscottaustin@gmail.com>
2019-10-02 22:43:33 -0700

    double jump and tweaking jump physics

--- Assets/2DPlatformer/Scripts/PlatformerPlayerController.cs
+++ Assets/2DPlatformer/Scripts/PlatformerPlayerController.cs
@@ -6,6 +6,13 @@ public class PlatformerPlayerController : PhysicsObject
 {
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
+    public float jumpSpeed = 2;
+    public float jumpTime = 0.4f;
+    public int numJumps = 1;
+
+    private float curJumpTime;
+    private bool lastJumped;
+    public int curNumJumps;
 
     private SpriteRenderer spriteRenderer;
     private Animator animator;
@@ -19,18 +26,31 @@ public class PlatformerPlayerController : PhysicsObject
     protected override void ComputeVelocity()
     {
         Vector2 move = Vector2.zero;
-
+        
         move.x = Input.GetAxis("Horizontal");
 
-        if(Input.GetButtonDown("Jump") && grounded) 
+        if(Input.GetButton("Jump"))
         {
-            velocity.y = jumpTakeOffSpeed;
-        } else if (Input.GetButtonUp("Jump"))
-        {
+            curJumpTime += Time.deltaTime;
+            if ((lastJumped && curJumpTime < jumpTime) || (curNumJumps < numJumps && !lastJumped)) {
+                if(!lastJumped) {
+                    lastJumped = true;
+                    curJumpTime = 0; 
+                    curNumJumps++;
+                    if(curNumJumps >= numJumps) lastJumped = true;
+                    velocity.y = jumpTakeOffSpeed;
+                }
+                velocity.y += jumpSpeed;
+            }
+            
+        } else if (Input.GetButtonUp("Jump")) {
+            lastJumped = false;
             if(velocity.y > 0)
             {
                 velocity.y *= 0.5f;
             }
+        } else {
+            if (grounded) curNumJumps = 0;
         }
         bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < -0.01f));
 
